e11a57ea267c53ead7c6e95a8f569da1d8127f5c
NinjaXz <Ninja1337x@protonmail.com>
2026-06-07 11:48:56 +0400

    Update BuildRelease.bat

--- BuildRelease.bat
+++ BuildRelease.bat
@@ -8,6 +8,6 @@ dotnet build Skua.sln -c Release -p:WarningLevel=0 --nologo
 echo =========================================
 echo Build Complete!
 echo You can find your build inside
-echo the newly created Build folder!
+echo the newly created /Build folder!
 echo =========================================
 pause
